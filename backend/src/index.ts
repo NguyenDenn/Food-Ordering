c782ae89765da6985886674e6ca38e3dff76c414
DoanDatt <122787025+DoanDatt@users.noreply.github.com>
2025-04-10 15:40:11 +0700

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/test", async (req: Request, res: Response) => {
+  res.json({ message: "Hello World" });
+});
+
+app.listen(3000, () => {
+  console.log(`Server is running on http://localhost:3000`);
+});
